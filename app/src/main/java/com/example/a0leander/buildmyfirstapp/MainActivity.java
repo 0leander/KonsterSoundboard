61f2f52a9906897e80ee5f9c1545757c5516bc0e
0leander <34345498+0leander@users.noreply.github.com>
2020-04-27 12:11:15 +0200

    add first mediaplayer implementation

--- app/src/main/java/com/example/a0leander/buildmyfirstapp/MainActivity.java
+++ app/src/main/java/com/example/a0leander/buildmyfirstapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.a0leander.buildmyfirstapp;
 
+import android.content.Context;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,10 +12,23 @@ public class MainActivity extends AppCompatActivity {
 
     public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
 
+    /*
+    media player testing
+    */
+    //create media player
+    MediaPlayer mediaPlayer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mediaPlayer = MediaPlayer.create(this, R.raw.testfile);
+    }
+
+
+    /** play testfile in mediaPlayer */
+    public void playTestFile(View view){
+        mediaPlayer.start();
     }
 
     /** Called when the user taps the Send button */
