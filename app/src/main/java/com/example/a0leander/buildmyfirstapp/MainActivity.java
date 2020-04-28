6a737dbcc42a44d175bd94467228225034ad3fc2
0leander <34345498+0leander@users.noreply.github.com>
2020-04-28 17:21:08 +0200

    fooling around with the recycler view

--- app/src/main/java/com/example/a0leander/buildmyfirstapp/MainActivity.java
+++ app/src/main/java/com/example/a0leander/buildmyfirstapp/MainActivity.java
@@ -22,6 +22,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /*
+        * Create media player
+        */
         mediaPlayer = MediaPlayer.create(this, R.raw.testfile);
     }
 
---
+++ app/src/main/java/com/example/a0leander/buildmyfirstapp/SoundButton.java
@@ -0,0 +1,6 @@
+package com.example.a0leander.buildmyfirstapp;
+
+public class SoundButton {
+
+
+}
